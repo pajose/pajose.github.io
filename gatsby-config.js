67f956982cf28d2d0931a320b0f12382deb7a57c
Patrick Jose <patrickgjose@gmail.com>
2024-09-22 22:18:23 -0700

    Update About Me section in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,24 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://pajose.github.io/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Patrick Jose',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Patrick Jose`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Software Engineer | Real-time Systems | Full-stack developer`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    // author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/pajose`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/patrick-jose/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `As a professional software engineer with over 5 years of experience, I have a strong foundation in developing real-time software systems. \
+            My expertise spans a range of programming languages and tools, including C++, Python, Java, and JavaScript, as well as cloud platforms like \
+            Google Cloud and AWS. I also have experience in other areas, including developing AI models, designing and implementing distributed systems, \
+            and creating web applications.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
